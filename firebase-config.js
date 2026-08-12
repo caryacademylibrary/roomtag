2be3b953b46c2c0d8e82932ec771b2e55ebdc1d1
caryacademylibrary <library@caryacademy.org>
2026-08-11 21:57:29 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,15 +9,9 @@
 // by the Firestore security rules described in README.md, not by hiding
 // this file.
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.__FIREBASE_CONFIG__ = {
   apiKey: "AIzaSyCrARY-bp-0c7TXP0mRhqOvY03kL4n_i0I",
   authDomain: "roommate-selections.firebaseapp.com",
   projectId: "roommate-selections",
@@ -26,7 +20,3 @@ const firebaseConfig = {
   appId: "1:318385361234:web:799dd9ce4ad17a260eaa8b",
   measurementId: "G-Z67RH60JQS"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
